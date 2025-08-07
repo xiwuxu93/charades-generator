af78377274a8751cc813686ddfbb07110164fce8
许锡钨 <773927425@qq.com>
2025-08-07 16:52:31 +0800

    feat:更新 sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -34,23 +34,5 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "weekly",
       priority: 0.7,
     },
-    {
-      url: `${baseUrl}/hard`,
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.6,
-    },
-    {
-      url: `${baseUrl}/animals`,
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.6,
-    },
-    {
-      url: `${baseUrl}/actions`,
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.6,
-    },
   ];
 }
